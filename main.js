b6f536ad951e491bf0bb4cb323d6fc063ba3529c
randomguy1920 <64695882+randomguy1920@users.noreply.github.com>
2020-05-03 14:55:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function renderHeader()
     var div = document.getElementById('header')
     div.innerHTML = `<h3>Report Bug on telegram at: @Anms1920 </h3>
   <p><a href ="https://randomguy1920.github.io/jshost/xxtn.zip">Download chrome extension </a></p>
-  <p><h5><a href ="https://randomguy1920.github.io/jshost/xxtn.pdf">Instructions to install </h5></a></p>
-<p><h5><a href ="https://randomguy1920.github.io/jshost/disclaimer.html">Important Info </h5></a></p>
+  <p><a href ="https://randomguy1920.github.io/jshost/xxtn.pdf">Instructions to install</a></p>
+<p><a href ="https://randomguy1920.github.io/jshost/disclaimer.html">Important Info</a></p>
   <h4>Beta v 1.1</h4>`
 }
